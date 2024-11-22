ea0fc8286122b1654542c319cf27cb339ce8216d
Mark Clemence Aratan <aratanmarkclemence@gmail.com>
2024-11-22 22:32:39 +0800

    Opening Page Changed

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:bluemart/pages/home_page.dart';
 
-class StartPage extends StatelessWidget{
-  const StartPage({super.key});
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
 
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,19 +20,39 @@ class StartPage extends StatelessWidget{
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Image.asset(
-                  'lib/images/KICK IT LOGO.png', 
-                  height: 350,
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset(
+                    'lib/images/KICK IT LOGO.png', 
+                    height: 350,
+                  ),
+                ),
+              
+              const SizedBox(height: 8,),
+
+              GestureDetector(
+                onTap: () => Navigator.push(context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  )
+                ),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white
+                    ),
+                    padding: const EdgeInsets.all(25),
+                    child: const Center(
+                      child: Text('START BROWSING',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ),
                 ),
               ),
-
-              const SizedBox(height: 20),
-
-              
             ],
           ),
-          ),
+        ),
       ),
     );
   }
